f95c2098ce87a019698e02292cd49f163b2b4bb3
Devon Wolfkiel <devonwolf@tutanota.com>
2021-06-05 18:31:56 -0700

    Itty bitty server and app file setup with correct typing and imports/exports

---
+++ src/lib/app.ts
@@ -0,0 +1,8 @@
+// const express = require('express');
+import * as express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,5 @@
+import app from './lib/app';
+
+app.listen(7890, () => {
+	console.log('Started on 7890');
+});
