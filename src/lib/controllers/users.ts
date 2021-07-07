89caf9b8a7d10ab213479054b8a258ff7e1f60fb
Devon Wolfkiel <devonwolf@tutanota.com>
2021-07-07 12:38:04 -0700

    Update lintfile and clean up tsconfig

--- src/lib/controllers/users.ts
+++ src/lib/controllers/users.ts
@@ -1,23 +1,19 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Router } from 'express';
 
 export default Router()
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
     .post('/', (req, res, next) => {
         console.log('POST request');
     })
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
     .get('/', (req, res, next) => {
         console.log('GET request');
     })
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
     .get('/:id', (req, res, next) => {
         console.log(`GET request for id ${req.params.id}`);
     })
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
     .put('/:id', (req, res, next) => {
         console.log(`PUT request for id ${req.params.id}`);
     })
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
     .delete('/:id', (req, res, next) => {
         console.log(`DELETE request for id ${req.params.id}`);
     });
