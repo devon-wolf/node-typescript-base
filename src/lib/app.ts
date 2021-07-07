890357546de2aab99d3deaf91ef23c3726b10dd3
Devon Wolfkiel <devonwolf@tutanota.com>
2021-07-07 14:18:34 -0700

    Updated routes

--- src/lib/app.ts
+++ src/lib/app.ts
@@ -1,12 +1,9 @@
 import express from 'express';
-import usersController from './controllers/users';
+import testRoutes from './controllers/tests';
 
 const app = express();
 
 app.use(express.json());
-app.use('/api/v1/users', usersController);
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-app.get('/', (req, res, next) => ('<h1>YOU GOT ME</h1>'));
+app.use('/api/v1/tests', testRoutes);
 
 export default app;
