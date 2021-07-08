e9d0e2c19b606fc7b17ac4dcd0f741aa1cce6379
Devon Wolfkiel <devonwolf@tutanota.com>
2021-07-07 18:14:13 -0700

    Add middleware and update types

--- src/lib/app.ts
+++ src/lib/app.ts
@@ -1,9 +1,14 @@
 import express from 'express';
+import { handleError, handleNotFound } from './middleware/errorHandling';
 import testRoutes from './controllers/tests';
 
 const app = express();
 
 app.use(express.json());
+
 app.use('/api/v1/tests', testRoutes);
+
+app.use(handleNotFound);
+app.use(handleError);
 
 export default app;
--- src/types.ts
+++ src/types.ts
@@ -6,3 +6,7 @@ export abstract class CRUDModel {
 	public abstract update(req : Request, res : Response) : void;
 	public abstract delete(req : Request, res : Response) : void;
 }
+
+export class ExpressError extends Error {
+	status : number | undefined;
+}
