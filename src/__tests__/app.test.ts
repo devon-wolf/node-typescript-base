bdffeab1d6ffba8db5cedb7cb337bf5f406397f4
Devon Wolfkiel <devonwolf@tutanota.com>
2021-07-08 20:05:45 -0700

    Test still not working

--- src/__tests__/app.test.ts
+++ src/__tests__/app.test.ts
@@ -1,13 +1,13 @@
 
-import pool from '../lib/utils/pool';
-import setup from '../data/setup';
+// import pool from '../lib/utils/pool';
+// import setup from '../data/setup';
 import request from 'supertest';
 import app from '../lib/app';
 
 describe('test routes', () => {
-    beforeEach(() => {
-        return setup(pool);
-    });
+    // beforeEach(() => {
+    //     return setup(pool);
+    // });
 
     it('makes a GET request', async () => {
         const response = await request(app).get('/api/v1/tests');
