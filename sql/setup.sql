de3a018caac9b3999bc53275102c3652714cf252
Devon Wolfkiel <devonwolf@tutanota.com>
2021-07-08 20:00:21 -0700

    Add some console logging to the setup-db file

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,1 +1,9 @@
 -- parent folder (/sql) needs to be on the same directory level as /src and /dist in order to be accessible --
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+	id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	email VARCHAR(512) NOT NULL,
+	hash VARCHAR(512) NOT NULL
+);
