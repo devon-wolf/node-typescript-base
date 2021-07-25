2aea2f1ec7d6d78c7cc25dfff3b857153540f431
Devon Wolfkiel <devonwolf@tutanota.com>
2021-07-24 20:56:29 -0700

    Remove tests for example routes

--- src/__tests__/app.test.ts
+++ src/__tests__/app.test.ts
@@ -1,44 +1,12 @@
 import pool from '../lib/utils/pool';
 import setup from '../data/setup';
-import request from 'supertest';
-import app from '../lib/app';
-
-const seedExample = {
-    exampleName: 'seed',
-    exampleBody: 'this is the seed data'
-};
 
 describe('test routes', () => {
     beforeEach(async () => {
         await setup(pool);
-        await request(app)
-            .post('/api/v1/examples')
-            .send(seedExample);
     });
 
-    it('POSTs a new example', async () => {
-        const newExample = {
-            exampleName: 'A Name',
-            exampleBody: 'A Body'
-        };
-
-        const response = await request(app)
-            .post('/api/v1/examples')
-            .send(newExample);
-      
-        expect(response.body).toEqual({
-            ...newExample,
-            id: '2'
-        });
-    });
-
-    it('makes a GET request', async () => {
-        const response = await request(app)
-            .get('/api/v1/examples');
-      
-        expect(response.body).toEqual([{
-            ...seedExample,
-            id: '1'
-        }]);
+    it('is a placeholder test that will pass', () => {
+        expect(2 + 2).toEqual(4);
     });
 });
